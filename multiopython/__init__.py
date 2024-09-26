8ca326a66b4b4665a7ee71d62f0a9fe443286ffa
Harrison Cook <harrison.cook@ecmwf.int>
2024-09-26 14:40:52 +0000

    Move to pyproject toml

--- multiopython/__init__.py
+++ multiopython/__init__.py
@@ -2,4 +2,4 @@ from .lib import MultioException  # noqa: F401
 from .metadata import Metadata  # noqa: F401
 from .multio import Multio  # noqa: F401
 
-__version__ = "0.0.1"
+from ._version import __version__ as __version__
