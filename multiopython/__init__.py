233f436240aa0b1707012592a098535f4acfe841
Harrison Cook <harrison.cook@ecmwf.int>
2024-09-27 07:43:26 +0000

    Fix isort

--- multiopython/__init__.py
+++ multiopython/__init__.py
@@ -1,5 +1,4 @@
+from ._version import __version__ as __version__
 from .lib import MultioException  # noqa: F401
 from .metadata import Metadata  # noqa: F401
 from .multio import Multio  # noqa: F401
-
-from ._version import __version__ as __version__
